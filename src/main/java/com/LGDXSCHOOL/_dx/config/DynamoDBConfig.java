264b95d2cf65697346c3e6fb1690966ef29f8cec
djbrachio@gmail.com <djbrachio@gmail.com>
2024-11-21 16:07:21 +0900

    채팅기능구현4 AWS 정보 암호화

---
+++ src/main/java/com/LGDXSCHOOL/_dx/config/DynamoDBConfig.java
@@ -0,0 +1,38 @@
+package com.LGDXSCHOOL._dx.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+
+@Configuration
+public class DynamoDBConfig {
+
+    @Value("${aws.accessKey}")
+    private String awsAccessKey;
+
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        // DynamoDB 클라이언트 생성
+        return DynamoDbClient.builder()
+                .region(Region.AP_NORTHEAST_2) // DynamoDB 리전 설정
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(awsAccessKey, awsSecretKey)
+                ))
+                .build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient)
+                .build();
+    }
+}
